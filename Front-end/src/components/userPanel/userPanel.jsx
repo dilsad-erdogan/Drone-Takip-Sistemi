78e13c57ce53f7caab6c1d6b87222a30cb75c9bd
dilsad-erdogan <dilsadrukiye@hotmail.com>
2023-11-30 22:29:22 +0300

    new commit

--- Front-end/src/components/userPanel/userPanel.jsx
+++ Front-end/src/components/userPanel/userPanel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Row, Card, Button, Container, Table } from 'react-bootstrap';
 
-const userPanel = ({ usersData }) => {
+const userPanel = ({ droneData }) => {
   return (
     <Container>
       <h2>Drones</h2>
@@ -10,16 +10,15 @@ const userPanel = ({ usersData }) => {
       <Card>
         <Card.Body>
           <Row>
-            {usersData.map(data => (
-                <Card key={data.user_id}>
-                  <Row>{data.user_id}</Row>
-                  <Row>{data.roletype_id}</Row>
-                  <Row>{data.name}</Row>
-                  <Row>{data.email}</Row>
-                  <Row>{data.password}</Row>
-                  <Row>{data.pilot_certificate}</Row>
-                  <Row>{data.drone_owner}</Row>
-                  <Row>{data.isActive}</Row>
+            {droneData.map(data => (
+                <Card key={data.drone_id}>
+                  <Row><bolt>Drone ID: </bolt>{data.drone_id}</Row>
+                  <Row><bolt>Drone Information ID: </bolt>{data.droneinfo_id}</Row>
+                  <Row><bolt>Drone Active: </bolt>{data.isactive}</Row>
+                  <Row><bolt>Drone Latitude: </bolt>{data.latitude}</Row>
+                  <Row><bolt>Drone Longitude: </bolt>{data.longitude}</Row>
+                  <Row><bolt>Drone Owner ID: </bolt>{data.owner_id}</Row>
+                  <Row><bolt>Drone Serial Number: </bolt>{data.serialnumber}</Row>
                 </Card>
               ))}
           </Row>
