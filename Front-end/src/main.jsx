7da1c84a1b5147fbad58bfc2da91b48a73e0d993
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-03-16 15:12:52 +0300

    refresh commit

--- Front-end/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <App />
-  </>
-)
