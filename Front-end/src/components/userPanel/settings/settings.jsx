484fb9b02dc55bd69e61176f0be36d6091cd6546
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-03-23 17:52:04 +0300

    certificate permission

--- Front-end/src/components/userPanel/settings/settings.jsx
+++ Front-end/src/components/userPanel/settings/settings.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import '../../ui/panel.css';
+import CertificateModel from '../../../../../Back-end/connections/pilotCertificate';
+const certificateModel = new CertificateModel();
+import CertificatePermissionModel from '../../../../../Back-end/connections/certificatePermission';
+const certificatePermissionModel = new CertificatePermissionModel();
 
 const settings = () => {
+  const [certificateId, setCertificateId] = useState('');
+  const [certificate, setCertificate] = useState([]);
+
+  useEffect(() => {
+    const fetchCertificateData = async () => {
+      try{
+        await certificateModel.fetchCertificateData();
+        const data = certificateModel.getCertificate();
+
+        if(Array.isArray(data)){
+          setCertificate(data);
+        } else{
+          console.error('Hata getCertificate bir dizi döndürmedi.');
+        }
+      } catch(error) {
+        console.error('Error fetching certificate data:', error.message);
+        console.error('Full error:', error);
+      }
+    }
+
+    fetchCertificateData();
+  })
+
+  const addCertificate = () => {
+    const newCertificate = {
+      pilot_id: localStorage.getItem("userId"),
+      certificate_id: certificateId,
+      permission_status: false
+    };
+
+    certificatePermissionModel.addCertificatePermission(newCertificate).then(() => {
+      alert("Sertifika isteğiniz başarıyla yönlendirilmiştir.");
+    }).catch((error) => {
+      alert("İzin sırasında bir hata oluştu." + error);
+    });
+  }
+
   return (
-    <div>settings</div>
+    <div className='topPanel'>
+      <div className='top'>
+        <div style={{fontSize: 20}}>Select your pilot certificate</div>
+        <button className='btn btn-outline-light' onClick={() => {addCertificate()}}>Add Certificate</button>
+      </div>
+
+      <div className='addForm'>
+        <select name='cat' id='id' onChange={(e) => {setCertificateId(e.target.value)}}>
+          <option>Select a certificate</option>
+          {certificate && certificate.map((data) => (
+            data.is_active === true ? (<option key={data.certificate_id} value={data.certificate_id}>{data.certificate_name}</option>) : (console.log())
+          ))}
+        </select>
+        <input type='file'></input>
+      </div>
+      
+    </div>
   )
 }
 
