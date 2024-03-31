849aa24b57d0ba4cc46857fc0deab73120075706
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-03-31 14:27:17 +0300

    search update

--- Front-end/src/components/ui/commonUsage/search.jsx
+++ Front-end/src/components/ui/commonUsage/search.jsx
@@ -1,11 +1,28 @@
 import '../panel.css'
 import { MdSearch } from 'react-icons/md';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const search = ({ placeholder }) => {
+  const [searchValue, setSearchValue] = useState("");
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    setSearchValue(params.get("q") || "");
+  }, [location]);
+
+  const handleSearch = (e) => {
+    const params = new URLSearchParams(location.search);
+    params.set("q", e.target.value);
+    navigate(`${location.pathname}?${params}`);
+  };
+  
   return (
     <div className='search'>
         <MdSearch></MdSearch>
-        <input type='text' placeholder={placeholder} className='searchInput'></input>
+        <input type='text' placeholder={placeholder} className='searchInput' value={searchValue} onChange={handleSearch}></input>
     </div>
   )
 }
